28bdf8d8f96495477cf86d99c79c2bce90731a14
Wassim Zerouta <wassim.zerouta@gmail.com>
2023-03-26 15:33:24 +0200

    Set main header

--- NewsApp/Controllers/ViewController.swift
+++ NewsApp/Controllers/ViewController.swift
@@ -7,13 +7,83 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
+    
+    let headerView = UIView()
+    let separation = UIView()
+    let scrollView = UIScrollView()
+    
+    let colorPrimaryDark = UIColor(red: 0.13, green: 0.37, blue: 0.38, alpha: 1.00)
+    let colorPrimary = UIColor(red: 0.41, green: 0.65, blue: 0.68, alpha: 1.00)
+    let colorGreen = UIColor(red: 0.02, green: 1.00, blue: 0.54, alpha: 1.00)
+    let colorRed = UIColor(red: 1.00, green: 0.03, blue: 0.17, alpha: 1.00)
+    let colorViolet = UIColor(red: 1.00, green: 0.03, blue: 0.17, alpha: 1.00)
+    let colorOrange = UIColor(red: 1.00, green: 0.27, blue: 0.00, alpha: 1.00)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        createHeaderView()
+        createFavoritesSection()
         // Do any additional setup after loading the view.
     }
-
+    
+    // Definition of the UI
+    
+    private func createHeaderView() {
+        let title = UILabel()
+        self.view.addSubview(headerView)
+        headerView.addSubview(title)
+        self.view.addSubview(separation)
+        headerView.backgroundColor = colorPrimary
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        headerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        headerView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        headerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        headerView.heightAnchor.constraint(equalToConstant: view.frame.height/7).isActive = true
+        
+        separation.translatesAutoresizingMaskIntoConstraints = false
+        separation.backgroundColor = colorPrimaryDark
+        separation.topAnchor.constraint(equalTo: headerView.bottomAnchor).isActive = true
+        separation.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        separation.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        separation.heightAnchor.constraint(equalToConstant: 5).isActive = true
+        
+        title.translatesAutoresizingMaskIntoConstraints = false
+        title.text = "NEWSLY"
+        title.font = UIFont(name:"Futura-Bold", size: 25.0)
+        title.textColor = UIColor.white
+        title.centerXAnchor.constraint(equalTo: headerView.centerXAnchor).isActive = true
+        title.centerYAnchor.constraint(equalTo: headerView.centerYAnchor).isActive = true
+        
+    }
+    
+    private func createFavoritesSection() {
+        self.view.addSubview(scrollView)
+        scrollView.isUserInteractionEnabled = true
+        scrollView.backgroundColor = colorPrimary
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.topAnchor.constraint(equalTo: separation.bottomAnchor).isActive = true
+        scrollView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        scrollView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        scrollView.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        
+        let v = UIView()
+        scrollView.addSubview(v)
+        v.backgroundColor = UIColor.red
+        v.translatesAutoresizingMaskIntoConstraints = false
+        v.centerYAnchor.constraint(equalTo: scrollView.centerYAnchor).isActive = true
+        v.leftAnchor.constraint(equalTo: scrollView.leftAnchor).isActive = true
+        v.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        v.widthAnchor.constraint(equalToConstant: 700).isActive = true
+        
+        let a = UIView()
+        v.addSubview(a)
+        a.backgroundColor = UIColor.yellow
+        a.translatesAutoresizingMaskIntoConstraints = false
+        a.leftAnchor.constraint(equalTo: v.leftAnchor, constant: 50).isActive = true
+        a.widthAnchor.constraint(equalToConstant: 500).isActive = true
+        a.heightAnchor.constraint(equalToConstant: 10).isActive = true
+    }
 
 }
 
