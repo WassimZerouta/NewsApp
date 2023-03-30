ee0ebadb292da0ffae5872a53899b579f21662c7
Wassim Zerouta <wassim.zerouta@gmail.com>
2023-03-30 13:52:05 +0200

    Fetch data from news API

---
+++ NewsApp/Models/NewsAPIResult.swift
@@ -0,0 +1,31 @@
+//
+//  NewsAPIResult.swift
+//  NewsApp
+//
+//  Created by Wass on 30/03/2023.
+//
+
+import Foundation
+
+struct NewsAPIResult: Decodable {
+    
+    var articles: [Article]
+}
+
+struct Article: Decodable {
+    
+    var source: Source
+    var author: String
+    var title: String
+    var description: String
+    var url: String
+    var urlToImage: String
+    var publishedAt: String
+    var content: String
+}
+
+struct Source: Decodable {
+    
+    var name: String
+}
+
