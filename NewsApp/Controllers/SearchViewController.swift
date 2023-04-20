85c0d8eb7dcd2be795c2d223b117abd7ecbd3029
Wassim Zerouta <wassim.zerouta@gmail.com>
2023-04-20 14:07:59 +0200

    Finalize de search fonctionality in searchViewController

--- NewsApp/Controllers/SearchViewController.swift
+++ NewsApp/Controllers/SearchViewController.swift
@@ -10,6 +10,9 @@ import Lottie
 
 class SearchViewController: UIViewController {
     
+    var favoriteSubject = CD_FavoriteSubject(context: CoreDataStack.sharedInstance.viewContext)
+    var cd_favoriteSubject = [CD_FavoriteSubject]()
+    
     var searchArray = [String]()
     
     let searchBar: UISearchBar = {
@@ -26,7 +29,7 @@ class SearchViewController: UIViewController {
         animation.contentMode = .scaleToFill
         animation.loopMode = .playOnce
         animation.animationSpeed = 1.5
-        animation.alpha = 0.3
+        animation.alpha = 0.5
         return animation
     }()
     
@@ -50,8 +53,6 @@ class SearchViewController: UIViewController {
         var collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
         return collection
     }()
-
-    var sections = ["FOOT", "BASKET", "VOLLEY", "MAHREZ"]
     
     let colorPrimary = UIColor(red: 0.41, green: 0.65, blue: 0.68, alpha: 1.00)
     
@@ -63,7 +64,14 @@ class SearchViewController: UIViewController {
         createFavoriteSubjectLabel()
         createAddFavoriteSubjectButton()
         createFavoritesSection()
-        createNewsAnimation()
+        collectionView.backgroundView = animation
+        animation.play()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        refreshCollectionView()
+        
     }
     
     func createSearchBar() {
@@ -94,9 +102,8 @@ class SearchViewController: UIViewController {
         addFavoriteSubjectButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
         addFavoriteSubjectButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
         addFavoriteSubjectButton.addTarget(self, action: #selector(addFavoriteSubjectButtonPressed), for: .touchUpInside)
-        
     }
-    
+        
     private func createFavoritesSection() {
         self.view.addSubview(collectionView)
         collectionView.isUserInteractionEnabled = true
@@ -105,18 +112,6 @@ class SearchViewController: UIViewController {
         collectionView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
         collectionView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
         collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
-    }
-    
-    func createNewsAnimation() {
-        self.view.addSubview(animation)
-        animation.isUserInteractionEnabled = true
-        animation.translatesAutoresizingMaskIntoConstraints = false
-        animation.centerYAnchor.constraint(equalTo: collectionView.centerYAnchor).isActive = true
-        animation.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        animation.widthAnchor.constraint(equalToConstant: 300).isActive = true
-        animation.heightAnchor.constraint(equalToConstant: 300).isActive = true
-        animation.play()
-        
     }
     
     func setupCollectionView() {
@@ -129,8 +124,11 @@ class SearchViewController: UIViewController {
         let alertController = UIAlertController(title: "Add new favorite subject !", message: nil, preferredStyle: .alert)
                 let confirmAction = UIAlertAction(title: "Add", style: .default) { (_) in
                     if let txtField = alertController.textFields?.first, let text = txtField.text {
-                        self.sections.append(text)
-                        self.collectionView.reloadData()
+                            // create entity instance with context
+                        self.favoriteSubject = CD_FavoriteSubjectRepository().saveFavoriteSubject(name: text, isAdded: true, completion: {
+                            print("\(text) is added")
+                        })
+                        self.refreshCollectionView()
                     }
                 }
                 let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (_) in }
@@ -142,33 +140,70 @@ class SearchViewController: UIViewController {
                 self.present(alertController, animated: true, completion: nil)
         
     }
+    
+    func refreshCollectionView() {
+        CD_FavoriteSubjectRepository().getFavoriteSubject { CD_FavoriteSubject in
+            DispatchQueue.main.async {
+                self.cd_favoriteSubject = CD_FavoriteSubject
+                self.cd_favoriteSubject.removeAll { cd_favoriteSubject in
+                    cd_favoriteSubject.name == nil
+                }
+                self.collectionView.reloadData()
+            }
+        }
+    }
 }
 
 extension SearchViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchArray.append(searchBar.text!)
         print(searchArray)
-        searchBar.text = ""
-        searchBar.endEditing(false)
+        let vc = MainViewController()
+        vc.subject = searchBar.text
+        let navVc = UINavigationController(rootViewController: vc)
+        present(navVc, animated: true)
+       
+        
     }
 }
 
 extension SearchViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        sections.count
+        cd_favoriteSubject.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CustomCollectionViewCell", for: indexPath) as! CustomCollectionViewCell
-        cell.titleLabel.text = sections[indexPath.row]
+        cell.titleLabel.text = cd_favoriteSubject[indexPath.row].name
         cell.contentView.layer.cornerRadius = 7
         cell.contentView.backgroundColor = colorPrimary
         return cell
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width/3.5 - 5, height: 50)
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let alert = UIAlertController(title: "Remove from favorite", message: "You can always access your content by signing back in", preferredStyle: UIAlertController.Style.alert)
+
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default, handler: { _ in
+            
+        }))
+            alert.addAction(UIAlertAction(title: "Delete",
+                                          style: UIAlertAction.Style.destructive,
+                                          handler: {(_: UIAlertAction!) in
+                
+                for favorite in self.cd_favoriteSubject {
+                    if favorite.name == self.favoriteSubject.name {
+                        CD_FavoriteSubjectRepository().removeFavoriteSubject(favoriteSubject: self.favoriteSubject) {
+                            print("Delete")
+                        }
+                        self.refreshCollectionView()
+                    }
+                }
+            }))
+            self.present(alert, animated: true, completion: nil)
     }
     
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width/3.5 - 5, height: 50)
+    }
 }
